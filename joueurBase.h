febd447abb822b4ff49f70c5ce144f8e730350be
developpmentsGit <devloppmentsGit@poo.com>
2019-12-13 19:11:28 +0100

    declaration et definition classe joueurBase

--- joueurBase.h
+++ joueurBase.h
@@ -2,12 +2,23 @@
 #define JOUEURBASE_H
 
 #include "joueur.h"
+#include "localisation.h"
+
 
 class joueurBase : public joueur
 {
     public :
-        joueurBase() = default ;
-        virtual bool deplace(int x, int y) override ;
+        joueurBase(const localisation &pos);
+
+        virtual bool deplaceEn(int x, int y) override ;
+
+        virtual void tuer() override;
+        virtual bool estVivant() const override;
+        virtual localisation position() const override;
+
+    private:
+        bool d_vivant;
+        localisation d_coordonnee;
 };
 
-#endif // JOUEURBASE_H
+#endif // JOUEUR1_H
