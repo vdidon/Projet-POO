920f1a60f3afce377c849599dc9a419739dda059
developpmentsGit <devloppmentsGit@poo.com>
2019-12-14 11:59:56 +0100

    declaration et implementation du constructeur qui prend en paramètre les coordonnées

--- localisation.cpp
+++ localisation.cpp
@@ -2,3 +2,6 @@
 
 localisation::localisation() : localisation {0,0}
 {}
+
+localisation::localisation(int x, int y) : d_x{x}, d_y{y}
+{}
--- localisation.h
+++ localisation.h
@@ -7,6 +7,13 @@ class localisation {
 public:
 ///Cionstructeur par défaut : point (0,0)
   localisation();
+  
+/**
+  Constructeur avec les coordonnées
+  @param x x-coordonnée
+  @param y y-coordonnée
+*/
+  localisation(int x, int y);
 
 private:
 
