505179881b8f46499c88581049ae096c788d52fc
Gaëtan <57009572+Gaetan1999@users.noreply.github.com>
2019-12-14 20:29:55 +0100

    Modifications mineurs
    Changement de certains noms dans la classe type du fichier Objet.h pour qu'ils soient plus parlant selon le type d'objet

--- objet.h
+++ objet.h
@@ -12,14 +12,14 @@ public:
 
 	class TYPES { //Class permettant de donner un type aux objets
 	public:
-		static constexpr char OBJET = 'O';
+		static constexpr char OBJET = 'O'; 		/** Pas utile je pense */
 		static constexpr char ROBOT = 'R';
-		static constexpr char ROBOT_ANCIEN = 'A';
-		static constexpr char ROBOT_NOUVEAU = 'N';
-		static constexpr char ROBOT_PERSO = 'A';
+		static constexpr char ROBOT_ANCIEN = 'RA'; /** Changement de nom, j'ai mis RA à la place de A - pour moi, plus parlant */
+		static constexpr char ROBOT_NOUVEAU = 'RN'; /** Changement de nom, j'ai mis RN à la place de N - pour moi, plus parlant */
+		static constexpr char ROBOT_PERSO = 'RP'; /** Changement de nom, j'ai mis RP à la place de A - pour moi, plus parlant */
 		static constexpr char JOUEUR = 'J';
-		static constexpr char JOUEUR_BASE = 'B';
-		static constexpr char JOUEUR_EXPERT = 'E';
+		static constexpr char JOUEUR_BASE = 'JB'; /** Changement de nom, j'ai mis JB à la place de B - pour moi, plus parlant */
+		static constexpr char JOUEUR_EXPERT = 'JE'; /** Changement de nom, j'ai mis JE à la place de E - pour moi, plus parlant */
 		static constexpr char MUR = 'M';
 		static constexpr char DEBRIS = 'D';
 		static constexpr char VIDE = ".";
