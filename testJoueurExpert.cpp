b8dacec674ad38edad852feb14e6fb3353b92cf5
developpmentsGit <devloppmentsGit@poo.com>
2019-12-14 16:11:10 +0100

    Sous Cas joueur est mort pour joueur Expert

--- testJoueurExpert.cpp
+++ testJoueurExpert.cpp
@@ -4,7 +4,15 @@
 
 TEST_CASE("Test des opérations du joueur expert")
 {
-    
+     SUBCASE("Le joueur est bien mort")
+    {
+        localisation pos{};
+        joueurExpert j1Expert {pos};
+
+        bool vivant = false;
+        j1Expert.tuer();
+        REQUIRE_EQ(j1Expert.estVivant(), vivant) ;
+    }
 }
 
 
