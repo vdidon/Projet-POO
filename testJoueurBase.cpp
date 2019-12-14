62d068dc50507427c7094e373298ebc987f2a779
developpmentsGit <devloppmentsGit@poo.com>
2019-12-14 16:03:58 +0100

    Cas de test pour joueur de base structure

---
+++ testJoueurBase.cpp
@@ -0,0 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN //permet d'eviter des erreurs au niveau de SUBCASE mais a inclure qu'une fois
+#include "doctest.h"
+#include "joueurBase.h"
+
+TEST_CASE("Test des opérations du joueur de base")
+{
+    
+}
+
+
