59ad0d9c5bba1b9106549d929cde04c757d8553d
developpmentsGit <devloppmentsGit@poo.com>
2019-12-14 11:49:41 +0100

    implémentation des méthodes specifique a un joueur

--- joueurExpert.cpp
+++ joueurExpert.cpp
@@ -7,3 +7,18 @@ bool joueurExpert::deplaceEn(int x, int y)
 {
 	d_coordonnee.moveTo(x,y);
 }
+
+void joueurExpert::tuer()
+{
+    d_vivant=false;
+}
+
+bool joueurExpert::estVivant() const
+{
+    return d_vivant;
+}
+
+localisation joueurExpert::position() const
+{
+    return d_coordonnee;
+}
--- joueurExpert.h
+++ joueurExpert.h
@@ -8,6 +8,10 @@ class joueurExpert : public joueur
     public :
         joueurExpert(const localisation &pos);
         virtual bool deplaceEn(int x, int y) override ;
+		
+		virtual void tuer() override ;
+        virtual bool estVivant() const override;
+        virtual localisation position() const override;
 };
 
 #endif // JOUEUREXPERT_H
