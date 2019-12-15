f25ae2ef872b1ec0c7e3f1d516d8f9105effcf3a
developpmentsGit <devloppmentsGit@poo.com>
2019-12-15 10:59:24 +0100

    Sous cas deplacement est correct pour joueur expert

--- testJoueurExpert.cpp
+++ testJoueurExpert.cpp
@@ -13,6 +13,54 @@ TEST_CASE("Test des opérations du joueur expert")
         j1Expert.tuer();
         REQUIRE_EQ(j1Expert.estVivant(), vivant) ;
     }
+	
+	SUBCASE("Le déplacement est correct")
+    {
+        localisation pos;
+        joueurExpert j1Expert{pos};
+        char choix;
+
+        int positionX1=j1Expert.position().x();
+        int positionY1=j1Expert.position().y();
+
+
+        int resultatX1=0;
+        int resultatY1=0;
+
+        int x1=0;
+        int y1=0;
+
+        bool ok=false;
+
+        while(ok==false)
+        {
+           joueur::interactionJoueurExpert(x1,y1);
+        //if(joueur::evalCase(const Terrain& gr,int lig,int col)
+        //{
+
+            resultatX1=positionX1+x1;
+            resultatY1=positionY1+y1;
+
+            double diffCoordx=abs(x1-positionX1);
+            double diffCoordy=abs(y1-positionY1);
+
+            if(diffCoordx==1&& positionY1==y1 || positionX1==x1&& diffCoordy==1)
+            {
+                j1Expert.deplaceEn(resultatX1,resultatY1);
+                ok=true;
+            }
+            else
+            {
+                joueur::echecMauvaisPas();
+            }
+
+        //}
+
+        }
+
+        REQUIRE_EQ(j1Expert.position().x(), resultatX1) ;
+        REQUIRE_EQ(j1Expert.position().y(), resultatY1) ;
+     }
 }
 
 
