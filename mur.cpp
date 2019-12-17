9b0791ea768584845dd136a995d9b034e34128e0
valère didon <vdidon@live.fr>
2019-12-17 16:19:46 +0100

    Méthode de objet déjà definie dans objet.cpp

--- mur.cpp
+++ mur.cpp
@@ -5,6 +5,4 @@ bool PeutAllerEn(int x, int y)
     return false; //Un mur ne bouge pas, donc pas besoin de vérifier. Mesure de sécurité
 }
 
-char objet::typeObjet() const {
-	return TYPES::OBJET; //Pas de TYPES::MUR donc c'est juste un objet (comme debris)
-}
+
