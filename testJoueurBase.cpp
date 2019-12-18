adda0830c31961cdad3ae5198188e3c034ecceb0
developpmentsGit <devloppmentsGit@poo.com>
2019-12-18 20:47:22 +0100

    modification des tests du joueur de base

--- testJoueurBase.cpp
+++ testJoueurBase.cpp
@@ -1,61 +1,52 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN //permet d'eviter des erreurs au niveau de SUBCASE mais a inclure qu'une fois
 #include "doctest.h"
 #include "joueurBase.h"
 
-TEST_CASE("Test des opérations du joueur de base")
+
+void leJoueurBasePeutEtreDeplacer(const joueurBase &jBase,int dx, int dy)
 {
-    SUBCASE("Le joueur est bien mort")
-    {
-        localisation pos;
-        joueurBase j1Base {pos};
-        bool vivant = false;
-        j1Base.tuer();
-        REQUIRE_EQ(j1Base.estVivant(), vivant) ;
-    }
-	
-	SUBCASE("Le déplacement est correct")
-    {
-        localisation pos;
-        joueurBase j1Base{pos};
-        char choix;
-
-        int positionX1=j1Base.position().x();
-        int positionY1=j1Base.position().y();
-
-        int resultatX1=0;
-        int resultatY1=0;
-
-        int x1=0;
-        int y1=0;
-        bool ok=false;
-
-        while(ok==false)
-        {
-            joueur::interactionJoueurBase(x1,y1);
-
-        //if(joueur::evalCase(const Terrain& gr,int lig,int col) //Teste si valeur pas en dehors du terrain
-        //{
-            resultatX1=positionX1+x1;
-            resultatY1=positionY1+y1;
-
-            int diffCoordx=abs(x1-positionX1);
-            int diffCoordy=abs(y1-positionY1);
-
-            if(diffCoordx<=1&& diffCoordy<=1) //test bien que le pas vaut 1
-            {
-                j1Base.deplaceEn(resultatX1,resultatY1);
-                ok=true;
-            }
-            else
-            {
-                joueur::echecMauvaisPas();
-            }
-
-        //}
-
-        }
-
-        REQUIRE_EQ(j1Base.position().x(), resultatX1) ;
-        REQUIRE_EQ(j1Base.position().y(), resultatY1) ;
-     }
+    bool JoueurBaseAutoriserDeplacement=jBase.PeutAllerEn(dx,dy);
+    REQUIRE_EQ(JoueurBaseAutoriserDeplacement, true);
 }
+
+TEST_CASE("Le deplacement du joueur de base est correct")
+{
+
+    joueurBase jBase1{};
+    SUBCASE("Le deplacement en diagonale est correct")
+    {
+        int dx=1;
+        int dy=1;
+        leJoueurBasePeutEtreDeplacer(jBase1,dx,dy);
+
+        dy=-1;
+        leJoueurBasePeutEtreDeplacer(jBase1,dx,dy);
+
+        dx=-1;
+        leJoueurBasePeutEtreDeplacer(jBase1,dx,dy);
+
+        dx=-1;
+        dy=1;
+        leJoueurBasePeutEtreDeplacer(jBase1,dx,dy);
+    }
+    SUBCASE("Le deplacement horizontale est correct")
+    {
+        int dx=-1;
+        int dy=0;
+        leJoueurBasePeutEtreDeplacer(jBase1,dx,dy);
+
+        dx=1;
+        leJoueurBasePeutEtreDeplacer(jBase1,dx,dy);
+    }
+
+    SUBCASE("Le deplacement verticale est correct")
+    {
+        int dx=0;
+        int dy=-1;
+        leJoueurBasePeutEtreDeplacer(jBase1,dx,dy);
+
+        dy=1;
+        leJoueurBasePeutEtreDeplacer(jBase1,dx,dy);
+    }
+}
+
+
