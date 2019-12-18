60b6d959887f06ca84b80a233332c98a4e02b75a
developpmentsGit <devloppmentsGit@poo.com>
2019-12-18 20:47:58 +0100

    modification des tests du joueur expert

--- testJoueurExpert.cpp
+++ testJoueurExpert.cpp
@@ -1,66 +1,35 @@
 #include "doctest.h"
 #include "joueurExpert.h"
 
-
-TEST_CASE("Test des opérations du joueur expert")
+void leJoueurExpertPeutEtreDeplacer(const joueurExpert &jExpert,int dx, int dy)
 {
-     SUBCASE("Le joueur est bien mort")
-    {
-        localisation pos{};
-        joueurExpert j1Expert {pos};
+    bool JoueurExpertAutoriserDeplacement=jExpert.PeutAllerEn(dx,dy);
+    REQUIRE_EQ(JoueurExpertAutoriserDeplacement, true);
+}
 
-        bool vivant = false;
-        j1Expert.tuer();
-        REQUIRE_EQ(j1Expert.estVivant(), vivant) ;
+TEST_CASE("Le deplacement du joueur expert est correct")
+{
+    joueurExpert jExpert1{};
+    SUBCASE("Le deplacement horizontale est correct")
+    {
+        int dx=-1;
+        int dy=0;
+        leJoueurExpertPeutEtreDeplacer(jExpert1,dx,dy);
+
+        dx=1;
+        leJoueurExpertPeutEtreDeplacer(jExpert1,dx,dy);
     }
-	
-	SUBCASE("Le déplacement est correct")
+
+    SUBCASE("Le deplacement verticale est correct")
     {
-        localisation pos;
-        joueurExpert j1Expert{pos};
-        char choix;
+        int dx=0;
+        int dy=-1;
+        leJoueurExpertPeutEtreDeplacer(jExpert1,dx,dy);
 
-        int positionX1=j1Expert.position().x();
-        int positionY1=j1Expert.position().y();
+        dy=1;
+        leJoueurExpertPeutEtreDeplacer(jExpert1,dx,dy);
+    }
 
-
-        int resultatX1=0;
-        int resultatY1=0;
-
-        int x1=0;
-        int y1=0;
-
-        bool ok=false;
-
-        while(ok==false)
-        {
-           joueur::interactionJoueurExpert(x1,y1);
-        //if(joueur::evalCase(const Terrain& gr,int lig,int col)
-        //{
-
-            resultatX1=positionX1+x1;
-            resultatY1=positionY1+y1;
-
-            double diffCoordx=abs(x1-positionX1);
-            double diffCoordy=abs(y1-positionY1);
-
-            if(diffCoordx==1&& positionY1==y1 || positionX1==x1&& diffCoordy==1)
-            {
-                j1Expert.deplaceEn(resultatX1,resultatY1);
-                ok=true;
-            }
-            else
-            {
-                joueur::echecMauvaisPas();
-            }
-
-        //}
-
-        }
-
-        REQUIRE_EQ(j1Expert.position().x(), resultatX1) ;
-        REQUIRE_EQ(j1Expert.position().y(), resultatY1) ;
-     }
 }
 
 
