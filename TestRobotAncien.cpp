b5ec147817c3948daafd8f44a1fc6fdc6927409b
valère didon <vdidon@live.fr>
2020-01-01 18:19:34 +0100

    Ajout des testes des robots

---
+++ TestRobotAncien.cpp
@@ -0,0 +1,36 @@
+#include "doctest.h"
+#include "robotAncien.h"
+
+TEST_CASE ("Tests pour robotAncien") {
+	robotAncien r = robotAncien{};
+
+	SUBCASE("typeObjet") {
+				REQUIRE_EQ(r.typeObjet(), objet::TYPES::ROBOT_ANCIEN);
+	}
+
+	SUBCASE("PeutAllerEn") {
+		SUBCASE("Directions autorisés") {
+					REQUIRE(r.PeutAllerEn(-1, 0));
+					REQUIRE(r.PeutAllerEn(0, -1));
+					REQUIRE(r.PeutAllerEn(1, 0));
+					REQUIRE(r.PeutAllerEn(0, 1));
+		}
+
+		SUBCASE("Directions non autorisés") {
+					REQUIRE_FALSE(r.PeutAllerEn(-1, -1));
+					REQUIRE_FALSE(r.PeutAllerEn(1, -1));
+					REQUIRE_FALSE(r.PeutAllerEn(1, 1));
+					REQUIRE_FALSE(r.PeutAllerEn(-1, 1));
+		}
+		SUBCASE("Déplacement de plus d'une case") {
+					REQUIRE_FALSE(r.PeutAllerEn(-2, 0));
+					REQUIRE_FALSE(r.PeutAllerEn(0, -2));
+					REQUIRE_FALSE(r.PeutAllerEn(2, 0));
+					REQUIRE_FALSE(r.PeutAllerEn(0, 2));
+					REQUIRE_FALSE(r.PeutAllerEn(2, 1));
+					REQUIRE_FALSE(r.PeutAllerEn(1, 2));
+		}
+	}
+}
+
+
---
+++ TestRobotNouveau.cpp
@@ -0,0 +1,33 @@
+#include "doctest.h"
+#include "robotNouveau.h"
+
+TEST_CASE ("Tests pour robotNouveau") {
+	robotNouveau r = robotNouveau{};
+
+	SUBCASE("typeObjet") {
+				REQUIRE_EQ(r.typeObjet(), objet::TYPES::ROBOT_ANCIEN);
+	}
+
+	SUBCASE("PeutAllerEn") {
+		SUBCASE("Directions autorisés") {
+					REQUIRE(r.PeutAllerEn(-1, 0));
+					REQUIRE(r.PeutAllerEn(0, -1));
+					REQUIRE(r.PeutAllerEn(1, 0));
+					REQUIRE(r.PeutAllerEn(0, 1));
+					REQUIRE(r.PeutAllerEn(-1, -1));
+					REQUIRE(r.PeutAllerEn(1, -1));
+					REQUIRE(r.PeutAllerEn(1, 1));
+					REQUIRE(r.PeutAllerEn(-1, 1));
+		}
+		SUBCASE("Déplacement de plus d'une case") {
+					REQUIRE_FALSE(r.PeutAllerEn(-2, 0));
+					REQUIRE_FALSE(r.PeutAllerEn(0, -2));
+					REQUIRE_FALSE(r.PeutAllerEn(2, 0));
+					REQUIRE_FALSE(r.PeutAllerEn(0, 2));
+					REQUIRE_FALSE(r.PeutAllerEn(2, 1));
+					REQUIRE_FALSE(r.PeutAllerEn(1, 2));
+		}
+	}
+}
+
+
